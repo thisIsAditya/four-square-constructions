08fea458aefe43a7d855850aebbbf144bae6fa9c
Akash <akashpandey.official28@gmail.com>
2021-07-28 01:18:23 +0530

    Regular Commit

--- admin/dashboard/header.inc.php
+++ admin/dashboard/header.inc.php
@@ -22,7 +22,7 @@
 </head>
 <body>  
 <header class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-  <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="../index.php">Four Square Constructions</a>
+  <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="../index.php">Four Square Constructions | Admin Dashboard</a>
   <div class="navbar-nav">
     <div class="nav-item text-nowrap">
       <a class="nav-link px-3" href="../logout.php">Sign out</a>
--- admin/editproduct.php
+++ admin/editproduct.php
@@ -2,10 +2,40 @@
 include("dashboard/session.inc.php");
 include("dashboard/header.inc.php");
 include("dashboard/sidepane.inc.php");
-?>
+include_once('../dbconnection.php');
+if (isset($_REQUEST['productUpdateBtn'])) {
+  //checking for empty feild
+  if (($_REQUEST['pr_name'] == "") || ($_REQUEST['pr_desc'] == "") || ($_REQUEST['pr_quantity'] == "") || ($_REQUEST['pr_unit'] == "") || ($_REQUEST['pr_origionalprice'] == "") || ($_REQUEST['pr_sellingprice'] == "") || ($_REQUEST['pr_brand'] == "")) {
+    $msg = ' <div class="alert alert-warning col-sm-6 ml-5 mt-2">All Fields Are Required....!</div> ';
+  } else {
+    $pr_name = $_REQUEST['pr_name'];
+    $pr_desc = $_REQUEST['pr_desc'];
+    $pr_quantity = $_REQUEST['pr_quantity'];
+    $pr_unit = $_REQUEST['pr_unit'];
+    $pr_origionalprice = $_REQUEST['pr_origionalprice'];
+    $pr_sellingprice = $_REQUEST['pr_sellingprice'];
+    $pr_brand = $_REQUEST['pr_brand'];
 
+    //uploading image
+    $pr_img = $_FILES['pr_img']['name'];
+    $pr_img_temp = $_FILES['pr_img']['tmp_name'];
+    $img_folder = "../image/productimg/" . $pr_img;
+    move_uploaded_file($pr_img_temp, $img_folder);
+
+    $sql = " INSERT INTO products(pr_name, pr_desc, pr_quantity, pr_img, pr_unit, pr_sellingprice, pr_origionalprice, pr_brand) 
+  VALUES ('$pr_name','$pr_desc','$pr_quantity','$img_folder','$pr_unit','$pr_sellingprice','$pr_origionalprice','$pr_brand') ";
+
+    if ($conn->query($sql) == TRUE) {
+      $msg = ' <div class="alert alert-success col-sm-6 ml-5 mt-2">product Added Successfully....!</div> ';
+      echo "<script>location.href='products.php';</script>";
+    } else {
+      $msg = ' <div class="alert alert-danger col-sm-6 ml-5 mt-2">Unable to Add product....!</div> ';
+    }
+  }
+}
+?>
 <!-- Body Starts -->
-<main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+    <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
       <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
         <h1 class="h2">Admin Dashboard</h1>
         <div class="btn-toolbar mb-2 mb-md-0">
@@ -22,58 +52,54 @@ include("dashboard/sidepane.inc.php");
 
       <!-- Body content starts -->
       <div class="container-fluid">
-      <p class="h2 text-center my-3"><strong>Edit Product</strong></p>
         <div class="row">
-          <div class="col-md-3">
-            <div class="m-3 p 2">
-              <img src="../img/demoProduct2.jpg" class="img-thumbnail" alt="Product Image">
-            </div>
-          </div>
-          
-          <div class="col-md-9 d-flex align-items-center">
-            <div class="m-3 p-2">
-              <p class="h2"><strong>Product ID</strong></p>
-              <p class="h4 text-muted">Product Category</p>
-            </div>
-          </div>
-
           <div class="col-lg-6">
-            <div class="m-2 p-2">
+            <div class="my-4 mx-3">
               <form action="" method="POST" enctype="multipart/form-data">
+                <h3 class="text-center">Add New Product</h3>
                 <div class="form-group mb-3">
-                  <label for="product_name">Products Name</label>
-                  <input type="text" class="form-control" id="product_name" name="product_name">
+                  <label for="pr_name">Products Name</label>
+                  <input type="text" class="form-control" id="pr_name" name="pr_name">
                 </div>
                 <div class="form-group mb-3">
-                  <label for="product_desc">Products Description</label>
-                  <textarea type="text" class="form-control" id="product_desc" name="product_desc" row="2"></textarea>
+                  <label for="pr_desc">Products Description</label>
+                  <textarea type="text" class="form-control" id="pr_desc" name="pr_desc" row="2"></textarea>
                 </div>
                 <div class="form-group mb-3">
-                  <label for="product_quantity">Products Quantity</label>
-                  <input type="text" class="form-control" id="product_quantity" name="product_quantity">
+                  <label for="pr_quantity">Products Quantity</label>
+                  <input type="text" class="form-control" id="pr_quantity" name="pr_quantity">
                 </div>
                 <div class="form-group mb-3">
-                  <label for="product_unit">Products Unit</label>
-                  <input type="text" class="form-control" id="product_unit" name="product_unit">
+                  <label for="pr_unit">Products Unit</label>
+                  <input type="text" class="form-control" id="pr_unit" name="pr_unit">
                 </div>
 
                 <div class="form-group mb-3">
-                  <label for="product_origional_price">Products Origional Price</label>
-                  <input type="text" class="form-control" id="product_origional_price" name="product_origional_price">
+                  <label for="pr_origionalprice">Products Origional Price</label>
+                  <input type="text" class="form-control" id="pr_origionalprice" name="pr_origionalprice">
                 </div>
                 <div class="form-group mb-3">
-                  <label for="product_price">Products Selling Price</label>
-                  <input type="text" class="form-control" id="product_price" name="product_price">
+                  <label for="pr_sellingprice">Products Selling Price</label>
+                  <input type="text" class="form-control" id="pr_sellingprice" name="pr_sellingprice">
                 </div>
-                <div class="form-group mb-4">
-                  <label for="product_brand">Products Brand</label>
-                  <input type="text" class="form-control" id="product_brand" name="product_brand">
+                <div class="form-group mb-3">
+                  <label for="pr_brand">Products Brand</label>
+                  <input type="text" class="form-control" id="pr_brand" name="pr_brand">
+                </div>
+                <div class="form-group my-5">
+                  <label for="pr_img">Products Image</label>
+                  <input type="file" class="form-control-file" id="pr_img" name="pr_img">
                 </div>
 
                 <div class="text_center my-3">
-                  <button type="submit" class="btn btn-danger" id="productSubmitBtn" name="productSubmitBtn">Submit</button>
+                  <button type="submit" class="btn btn-danger" id="productUpdateBtn" name="productUpdateBtn">Update</button>
                   <a href="products.php" class="btn btn-secondary">Close</a>
                 </div>
+                <?php
+                if (isset($msg)) {
+                echo $msg;
+                  }
+                ?>
               </form>
             </div>
           </div>
